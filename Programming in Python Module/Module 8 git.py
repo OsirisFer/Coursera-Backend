fa1de803e7ae98fd55c2847dff005c07ac8e5110
Osiris Fernández <osirisfernandez1999@gmail.com>
2025-12-30 12:12:38 -0300

    Add initial content for Unix commands and continuous integration overview

---
+++ Programming in Python Module/Module 8 git.py
@@ -0,0 +1,19 @@
+# Continuos Integration es una práctica de desarrollo de software donde los desarrolladores integran 
+# su código en un repositorio compartido con frecuencia,  preferiblemente varias veces al día. 
+# Cada integración es verificada por una construcción automatizada (incluyendo pruebas) para detectar errores lo antes posible.
+# Un ejemplo de esto es GitHub Actions, que permite automatizar flujos de trabajo como pruebas y despliegues.
+# Continuous Delivery resumido es dejar el código listo para producción en cualquier momento
+# Continuous Deployment va un paso más allá y automatiza el despliegue del código a producción después de pasar todas las pruebas.
+"""
+Comandos Unix:
+cd ..  # Moverse al directorio padre
+mkdir <nombre_directorio>  # Crear un nuevo directorio
+ls  # Listar archivos y directorios en el directorio actual
+pwd  # Mostrar la ruta del directorio actual
+man <comando>  # Mostrar el manual de un comando
+touch <nombre_archivo>  # Crear un nuevo archivo vacío
+clear # Limpiar la pantalla de la terminal
+mv <origen> <destino>  # Mover o renombrar un archivo o directorio
+
+
+"""
