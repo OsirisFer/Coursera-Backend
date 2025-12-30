eef3fd0ce4345e49639725114dae91e5776d3bce
Osiris Fernández <osirisfernandez1999@gmail.com>
2025-12-30 13:42:26 -0300

    Add additional Unix and Git command examples to Module 8

--- Programming in Python Module/Module 8 git.py
+++ Programming in Python Module/Module 8 git.py
@@ -14,6 +14,32 @@ man <comando>  # Mostrar el manual de un comando
 touch <nombre_archivo>  # Crear un nuevo archivo vacío
 clear # Limpiar la pantalla de la terminal
 mv <origen> <destino>  # Mover o renombrar un archivo o directorio
+cat <nombre_archivo>  # Mostrar el contenido de un archivo
+wc -l <nombre_archivo>  # Contar las líneas de un archivo
+less <nombre_archivo>  # Ver el contenido de un archivo página por página
+grep <patrón> <nombre_archivo>  # Buscar un patrón en un archivo
+pipes (|)  # Usar la salida de un comando como entrada para otro comando
+flags (-)  # Modificadores que cambian el comportamiento de los comandos
+
+GIT COMMANDS:
+git init  # Inicializar un nuevo repositorio Git
+git clone <url_repositorio>  # Clonar un repositorio remoto
+git status  # Ver el estado de los archivos en el repositorio
+git add <nombre_archivo>  # Agregar archivos al área de preparación (staging area)
+git commit -m "mensaje"  # Confirmar los cambios con un mensaje descriptivo
+git push  # Subir los cambios al repositorio remoto
+git pull  # Descargar y fusionar cambios del repositorio remoto
+git fetch  # Descargar cambios del repositorio remoto sin fusionarlos
+git branch  # Listar, crear o eliminar ramas
+git checkout -B <nombre_rama> # Cambiar a una rama o crear una nueva rama y cambiar a ella  
+git merge <nombre_rama>  # Fusionar una rama en la rama actual
+git log  # Ver el historial de commits
+git remote -v  # Remote lo que hace es listar los repositorios remotos vinculados al repositorio local
+
+El orden generalmente es: 
+git init -> git status -> git add -> git commit -> git pull -> git push
+
+
 
 
 """
